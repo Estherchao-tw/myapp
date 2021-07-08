d392e36c538fee245cf0601335f15f5d2e79fdff
root <git@github.com>
2021-07-08 08:30:00 +0000

    app

--- app.js
+++ app.js
@@ -4,9 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var registerRouter = require('./routes/member');
-var loginRouter = require('./routes/login');
+var indexRouter = require('./routes/member');
 var usersRouter = require('./routes/users');
 
 var app = express();
@@ -24,8 +22,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/register', registerRouter);
-app.use('/login', loginRouter);
 
 
 
